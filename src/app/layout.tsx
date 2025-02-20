7449a6327caaeace17c82b893247bf30a009f7b3
andre.junior <andrrejunior@hotmail.com.br>
2025-02-20 19:36:21 -0300

    chore: add eslint sort plugin and prettier tailwind plugin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
+import "./globals.css";
+
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
-import "./globals.css";
-import { Weight } from "lucide-react";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,14 +1,14 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
 const ProductsPage = () => {
-    return (
-        <div className="p-5 border border-red-500 rounded-xl">
-            <h1 className="text-red-500">Product Page</h1>
-            <Button>FSW 7.4</Button>
-            <Input placeholder="Bora fechar esse projeto!"/>
-        </div>
-    );
+  return (
+    <div className="rounded-xl border border-red-500 p-5">
+      <h1 className="text-red-500">Product Page</h1>
+      <Button>FSW 7.4</Button>
+      <Input placeholder="Bora fechar esse projeto!" />
+    </div>
+  );
 };
- 
+
 export default ProductsPage;
